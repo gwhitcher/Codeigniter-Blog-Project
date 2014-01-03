43e76a55657fd4c14c9751484f2a144f3bce3733
George Whitcher <george@georgewhitcher.com>
2014-01-03 18:58:41 -0500

    Ordered posts to latest at top

--- application/controllers/blog.php
+++ application/controllers/blog.php
@@ -18,9 +18,8 @@ class Blog extends CI_Controller {
 	$config['num_links'] = 5;
 	$config['full_tag_open'] = '<div id="pagination">';
 	$config['full_tag_close'] = '</div>';
-		
 	$this->pagination->initialize($config);
-		
+	$this->db->order_by("id","desc");
 	$query = $this->db->get('articles', $config['per_page'], $this->uri->segment(3));
 	$data["articles"] = $query->result_array();
 	$data['title'] = 'Home';
@@ -34,39 +33,34 @@ class Blog extends CI_Controller {
 	public function view($slug)
 	{
 	$data['article'] = $this->article_model->get_articles($slug);
-
 	if (empty($data['article']))
 	{
 		show_404();
 	}
-
 	$data['title'] = $data['article']['title'];
 	$this->load->view('template/header', $data);
 	$this->load->view('template/sidebar', $data);
 	$this->load->view('article_view', $data);
 	$this->load->view('template/footer');
 	}
-	
 	//LOAD CATEGORY
 	function category($slug) {
 	$this->load->library('pagination');
+	$this->db->order_by("id","desc");
 	$this->db->where('slug', $slug);
 	$data['category'] = $slug;
 	$data['category'] = $this->article_model->get_categories($slug);
 	if (empty($data['category']))
 	{
 		show_404();
-	}
-				
+	}			
 	$config['base_url'] = site_url('category/'.$slug.'/');
 	$config['total_rows'] = $this->db->get('articles')->num_rows();
 	$config['per_page'] = 5;
 	$config['num_links'] = 5;
 	$config['full_tag_open'] = '<div id="pagination">';
 	$config['full_tag_close'] = '</div>';
-		
-	$this->pagination->initialize($config);
-		
+	$this->pagination->initialize($config);	
 	$query = $this->db->where('category_id', $data['category']['id']);
 	$query = $this->db->get('articles', $config['per_page'], $this->uri->segment(3));
 	$data["articles"] = $query->result_array();
