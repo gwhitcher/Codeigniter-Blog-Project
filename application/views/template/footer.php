2afef637009a75c0b4cca23616eec08f33bb8afa
George Whitcher <george@georgewhitcher.com>
2014-01-15 21:40:35 -0500

    Dashboard link and logout added to footer for easy access to the dashboard.

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,5 +1,8 @@
 </section>
 <footer>
+<?php if($this->session->userdata('logged_in')) { ?>
+<a href="<?php echo base_url(); ?>admin">Admin</a> | <a href="<?php echo base_url(); ?>logout">Logout</a><br />
+<?php } ?>
 <strong><?php echo $this->config->item('title'); ?> is a Trademark of <?php echo $this->config->item('title'); ?>. 
 <br />
 &copy; Copyright <?php echo $this->config->item('copyrightyear'); ?> - <?php echo date("Y"); ?> <?php echo $this->config->item('title'); ?>. All Rights Reserved.</strong>
