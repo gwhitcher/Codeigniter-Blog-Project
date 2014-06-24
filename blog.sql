5019d271b67db71607f3b07479079f6cb396d297
gwhitcher <george@georgewhitcher.com>
2014-06-24 18:08:37 -0400

    Filled in missing fields in database for better installation.

--- blog.sql
+++ blog.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 25, 2014 at 12:02 AM
+-- Generation Time: Jun 25, 2014 at 12:07 AM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.11
 
