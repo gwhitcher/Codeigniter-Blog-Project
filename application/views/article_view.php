c8aafabef2ff45bbc0dd68d0f3f2f42689f65cb8
George Whitcher <george@georgewhitcher.com>
2017-04-21 10:07:39 -0400

    Removed DISQUS.  Oops!

--- application/views/article_view.php
+++ application/views/article_view.php
@@ -13,21 +13,5 @@
 <span class='st_email_large' displayText='Email'></span>
 <span class='st_sharethis_large' displayText='ShareThis'></span>
 </div>
-<div id="comments">
-<div id="disqus_thread"></div>
-    <script type="text/javascript">
-        /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-        var disqus_shortname = 'georgewhitcher'; // required: replace example with your forum shortname
-
-        /* * * DON'T EDIT BELOW THIS LINE * * */
-        (function() {
-            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-        })();
-    </script>
-    <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-    <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
-</div>
 </article>
 </div>
