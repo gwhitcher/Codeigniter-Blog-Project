19e4df7822b36777fc697c2b3d2faf76eabec62a
George Whitcher <george@georgewhitcher.com>
2017-04-21 10:10:55 -0400

    AdSense added.

--- application/views/template/default/footer.php
+++ application/views/template/default/footer.php
@@ -3,6 +3,18 @@
 </div>
 </div>
 <footer>
+    <div class="advertisement">
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <!-- Responsive -->
+        <ins class="adsbygoogle"
+             style="display:block"
+             data-ad-client="ca-pub-1862231357641748"
+             data-ad-slot="1935611714"
+             data-ad-format="auto"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+    </div>
 <?php echo base64_decode('RGV2ZWxvcGVkIGJ5IDxhIGhyZWY9Imh0dHA6Ly93d3cuZ2Vvcmdld2hpdGNoZXIuY29tIj5HZW9yZ2UgV2hpdGNoZXI8L2E+'); ?>
 </footer>
 </body>
--- application/views/template/default/sidebar.php
+++ application/views/template/default/sidebar.php
@@ -6,6 +6,16 @@
 <?php echo eval('?>' .$sidebar_item['body']. '<?php '); ?>
 </div>
 <?php } ?>
+    <script type="text/javascript">
+        google_ad_client = "ca-pub-1862231357641748";
+        google_ad_slot = "4889078117";
+        google_ad_width = 336;
+        google_ad_height = 280;
+    </script>
+    <!-- Sidebar -->
+    <script type="text/javascript"
+            src="//pagead2.googlesyndication.com/pagead/show_ads.js">
+    </script>
 </div>
 <div class="footer">&nbsp;</div>
 </div>
